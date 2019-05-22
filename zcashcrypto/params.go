e9660081b69f91cac1c2696581e37f02f87ecb50
ASC <asc@ASCs-iMac.local>
2019-05-22 15:37:36 -0500

    boo

--- zcashcrypto/params.go
+++ zcashcrypto/params.go
@@ -3,7 +3,7 @@ package zcashcrypto
 type NetworkId [2]byte
 type Prefix [1]byte
 
-var SecretKeyPrefix = Prefix{0x140}
+var SecretKeyPrefix = Prefix{140}
 
 var MainnnetId NetworkId = NetworkId{0x1C}
 var TestnetId NetworkId = NetworkId{0x1D, 0x25}
